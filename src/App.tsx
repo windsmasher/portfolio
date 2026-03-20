88f74e0bdb9ea3eb65fb8de64a9296f4527f9fbf
windsmasher <paczka94@gmail.com>
2026-03-20 11:45:41 +0100

    feat(main): first set up

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <header className="header">
+        <h1>Tomasz Paczka</h1>
+      </header>
+      <main className="main-content">
+        <h2>Page under construction</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
