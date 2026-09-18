3b75e3d8f8dc0095a3b45cae588a76b6954e4577
tomasz <Tomasz.Paczka@collinsongroup.com>
2026-09-18 15:32:35 +0200

    feat(main): favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,14 @@ export const metadata: Metadata = {
     default: siteTitle,
     template: `%s · ${siteTitle}`,
   },
+  icons: {
+    icon: [
+      { url: '/favicon-32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.png', sizes: '256x256', type: 'image/png' },
+    ],
+    shortcut: '/favicon.png',
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({
